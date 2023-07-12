ac7be6158169cbbe0f02b32985ffcd6470b065ca
RodLopezDev <rodrigolopezdev@gmail.com>
2023-07-12 10:54:02 -0500

    feat: :sparkles: added messagepattern on microservice

--- microservice/src/app.controller.ts
+++ microservice/src/app.controller.ts
@@ -4,14 +4,20 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { KAFKA_REQUEST_TOPIC } from './app/Constants';
 import { TransactionRequestDto } from './dto/TransactionRequestDto';
+import { TransactionResponseDto } from './dto/TransactionResponseDto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @MessagePattern(KAFKA_REQUEST_TOPIC)
-  validateTransaction(@Payload() message: TransactionRequestDto): void {
-    Logger.log('MESSAGE RECEIVED ON KAFKA_REQUEST_TOPIC TOPIC');
-    this.appService.validateTransaction(message);
+  validateTransaction(@Payload() message: TransactionRequestDto): any {
+    const { name } = message;
+    console.log(name, 'MESSAGE CONTENT');
+    const transactionResponseDto: TransactionResponseDto = {
+      value: name.includes('MY_NAME') ? 'OK' : 'ERROR',
+    };
+    const result = JSON.stringify(transactionResponseDto);
+    return result;
   }
 }
