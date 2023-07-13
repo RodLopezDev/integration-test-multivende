ae9b91574bd3d6fae75cac98d254027915ba6124
RodLopezDev <rodrigolopezdev@gmail.com>
2023-07-12 20:49:10 -0500

    feat: :sparkles: persons js exercice

---
+++ _otros-ejercicios/1-js-exercices/1-persons/index.js
@@ -0,0 +1,20 @@
+const persons = require("./persons.json");
+
+/** FILTER USERS WITH ADDERSS */
+const personsWithAddress = persons.filter(
+  (person) => !!person?.address?.length
+);
+
+/** SORT LIST ABOUT NAME */
+personsWithAddress.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+// /** FILTER AGE */
+const finalValue = personsWithAddress
+  .filter((person) => person.age > 20 && person.age < 30)
+  .filter((person) => {
+    const firstCharOnName = person.name[0];
+    return ["H", "L"].includes(firstCharOnName.toUpperCase());
+  });
+
+// FINAL RESULT
+console.log(finalValue);
